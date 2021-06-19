64a973073313091ceb99033269ae729e06ce19ad
Ahmed BASET <60217749+AhmedELGAMMUDI@users.noreply.github.com>
2021-06-19 10:06:59 +0300

    Update settings.py
    django

--- singup/settings.py
+++ singup/settings.py
@@ -29,9 +29,9 @@ ALLOWED_HOSTS = []
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'minimus.develop.team@gmail.com'
-EMAIL_HOST_PASSWORD = 'oeksncaobyimgzso'
-DEFAULT_FROM_EMAIL = 'Ahmed Elgammudi'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL=False 
